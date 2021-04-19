59a0f57e7516e6d4058fbece1774ba8ab98c5770
Sebastien Coppolani <sebcoppo@yahoo.fr>
2021-04-19 08:41:27 +0200

    App created

---
+++ src/components/app.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+import flats from '../../data/flats';
+import FlatList from './flat_list';
+import Marker from './marker';
+
+const GOOGLE_API_KEY = 'AIzaSyBAn30BxhuXUv5h-yIA88MuigpKBVNX6uM';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      flats,
+      selectedFlat: flats[0]
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact bootstrapURLKeys={{ key: GOOGLE_API_KEY }}
+                          defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
